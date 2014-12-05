8ed35f69e37ee6bd5015dd84c0df83d7168bcf08
zyearn <zhujiashun2010@gmail.com>
2014-12-05 13:12:25 +0800

    fix bug in client.js

--- client.js
+++ client.js
@@ -4,49 +4,48 @@ var http = require('http');
 
 socket.on('connect', function(){
     console.log('connected');
+});
     
-    socket.on('disconnect', function(){
-        console.log('disconnect');
-    });
+socket.on('disconnect', function(){
+    console.log('disconnect');
+});
+
+socket.on('req', function(req){
+    console.log('receive req');
+    console.log('method: ' + req.method);
     
-    socket.on('req', function(req){
-        console.log('receive req');
-        console.log('method: ' + req.method);
-        
-        //http request start
-        var options = {
-          host: 'localhost',
-          port: 8088,
-          path: req.url,
-          method: req.method,
-          headers: req.headers 
-        };
-        
-        var forwardreq = http.request(options, function(res){
-            console.log('in request callback');
-            var str = '';
+    //http request start
+    var options = {
+      host: 'localhost',
+      port: 8088,
+      path: req.url,
+      method: req.method,
+      headers: req.headers 
+    };
+    
+    var forwardreq = http.request(options, function(res){
+        console.log('in request callback');
+        var str = '';
 
-            res.on('data', function(chunk){
-                str += chunk;
-            });
+        res.on('data', function(chunk){
+            str += chunk;
+        });
 
-            res.on('end', function(){
-                console.log('str = ' + str);
-                console.log('typeof str = ' + typeof(str));
+        res.on('end', function(){
+            console.log('str = ' + str);
+            console.log('typeof str = ' + typeof(str));
 
-                socket.emit('res', {
-                    result: str,
-                    id:req.id
-                });
+            socket.emit('res', {
+                result: str,
+                id:req.id
             });
         });
-        
-        if (req.method == "PUT" || req.method == "POST") {
-            forwardreq.write(req.body); 
-        }
+    });
+    
+    if (req.method == "PUT" || req.method == "POST") {
+        forwardreq.write(req.body); 
+    }
 
-        forwardreq.end();
-        //http request end
-        
-    })
-});
+    forwardreq.end();
+    //http request end
+})
